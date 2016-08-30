c7abacb3b6d85b5d6a72d6ab7f7add853f294151
taka <masterkuzutt+github@gmail.com>
2016-08-30 20:12:40 +0900

    add knockout js 2

--- src/js/script.js
+++ src/js/script.js
@@ -1,107 +1,59 @@
+/**
+ * @description Represents  location data position and related data for google.maps.api
+ * @constructor
+ * @param  {object}  title :  'Initial Location' ,position :  {lat: 40.74135, lng: -73.99802}
+ */
+var Location = function (data) {
+  "use strict";
+  var self = this;
+  this.title = data.title;
+  this.lat = data.position.lat;
+  this.lng = data.position.lng;
+  this.position = data.position;
+  this.latLng = new google.maps.LatLng(data.position.lat,data.position.lng);
 
-
-// Model
-var Location = function (initialData) {
-  this.title = ko.observable(initialData.title);
-  this.lat = ko.observable(initialData.position.lat);
-  this.lng = ko.observable(initialData.position.lng);
-  this.position = function () {
-    return {lat : this.lat(),lng :this.lng()};
-  };
   this.marker = new google.maps.Marker({
-            title : this.title(),
-            position : this.position(),
+            title : this.title,
+            position : this.position,
             map : null ,
-            animation : google.maps.Animation.DROP,
-      });
-};
-
-//centerLocation {lat: 40.74135, lng: -73.99802}
-var googleMap = function  (mapElement,centerLocation,mapStyle) {
-
-  var mapApi =  google.maps,
-      self = this;
-  // init map
-  this.map  =  new mapApi.Map(mapElement, {
-    center: centerLocation,
-    zoom : 8,
-    styles : mapStyle,
-    mapTypeControl :false
+            animation : google.maps.Animation.DROP
   });
 
-  // init infoWindow
-  this.infoWindow = new mapApi.InfoWindow();
-  // init bounds
-  this.bounds = new mapApi.LatLngBounds();
+  this.streetViewLoc = null ;
 
-  // init markers array
-  this.markers = [];
-
-  // init locacion data
-  this.locationData = ko.observableArray([]);
-
-  /*
-   *
-   *
-   */
-  this.addLocationData = function  (location) {
-    this.locationData.push(ko.observable(new Location(location)));
+  /**
+  * @description update streetView location based on location parameter
+  */
+  this.updateStreetViewLoc = function () {
+    var streetViewService = new google.maps.StreetViewService();
+    streetViewService.getPanoramaByLocation(this.position, 50 ,function (data, status) {
+      if (status == google.maps.StreetViewStatus.OK){
+        self.streetViewLoc =  data.location.latLng;
+      } else {
+        self.streetViewLoc = null;
+      }
+    });
   };
 
-  /*
-   *
-   *
-   */
-  this.createMarkers = function () {
-    for ( var i = 0 ,len = this.locationData().length; i < len ; i++){
-
-      var marker = new mapApi.Marker({
-            title : this.locationData()[i]().title(),
-            position : this.locationData()[i]().position(),
-            map : this.map ,
-            animation : mapApi.Animation.DROP,
-            id : i
-      });
-      this.markers.push(marker);
-    }
-  };
-
-  /*
-   *
-   *
-   */
-  this.setAllMarkerMap = function () {
-    for ( var i = 0 , len = self.markers.length ; i < len ; i++) {
-      self.markers[i].setMap(self.map);
-      self.bounds.extend(self.markers[i].position);
-    }
-    self.map.fitBounds(self.bounds);
-  };
-
-  /*
-   *
-   *
-   */
-  this.setAllMarkerNull = function (){
-    for ( var i = 0 , len = self.markers.length ; i < len ; i++) {
-      self.markers[i].setMap(null);
-    }
+  /**
+  * @description update map property by parameter
+  * @param {object} google.maps.Map
+  */
+  this.setMap = function (map){
+    this.marker.setMap(map);
   };
 
 };
 
-
-var initLocationData  = [
+var INITIAL_LOCATION_DATA  = [
   { title :  'Initial Location' ,position :  {lat: 40.74135, lng: -73.99802} },
   { title :  'second  Location' ,position :  {lat: 40.74145, lng: -73.99812} },
   { title :  'third   Location' ,position :  {lat: 40.74155, lng: -73.99822} },
   { title :  'forth   Location' ,position :  {lat: 40.74165, lng: -73.99832} }
 ];
 
-
-
 //
-var styles = [
+var STYLES = [
           {
             featureType: 'water',
             stylers: [
@@ -168,67 +120,92 @@ var styles = [
           }
         ];
 
-// ViewModel
 
-function ViewModel () {
+/**
+ * @description Represents MV for location data and hmtl
+ * @constructor
+ */
+var ViewModel = function  () {
+ "use strict";
   var self = this;
-  /*
-   *  initMap()
-   *  It is called from google api callback parameter
-   */
-  this.initMap  =  function () {
-    var mapApi =  google.maps // google.maps;
+  // init map
+  this.map  =  new google.maps.Map($('#map')[0], {
+    center : INITIAL_LOCATION_DATA[0].position ,
+    zoom : 8,
+    styles : STYLES ,
+    mapTypeControl :false
+  });
+  // init infoWindow
+  this.infoWindow = new google.maps.InfoWindow();
+  // init bounds
+  this.bounds = new google.maps.LatLngBounds();
+  // init locacion data
+  this.locationData = ko.observableArray([]);
 
-    // init googleMap model
-    var Map = new googleMap($('#map')[0],initLocationData[0].position,styles);
-    ko.applyBindings(Map);
-    // add location data to map
-    for ( var i = 0, len = initLocationData.length; i < len ; i++){
-      // console.log(initLocationData[i]);
-      Map.addLocationData(initLocationData[i]);
+  /**
+  * @description [TODO] wirte comment
+  */
+  this.init  =  function () {
+
+    //init  location data
+    for ( var i = 0, len = INITIAL_LOCATION_DATA.length; i < len ; i++){
+     var location = new Location(INITIAL_LOCATION_DATA[i]);
+     location.marker.addListener('click',function () {
+        // self.setStreetVeiwToInfoWindow(Map.map,Map.infoWindow,marker,'pano');
+        console.log("clicked! : " , location);
+     });
+
+     this.locationData.push(location);
     }
 
-    Map.createMarkers();
-
-    for (var i = 0 ,len = Map.markers.length ; i <  len ; i++){
-      var marker = Map.markers[i];
-      marker.addListener('click',function () {
-        self.setStreetVeiwToInfoWindow(Map.map,Map.infoWindow,marker,'pano');
-      });
-    }
-
-    $('.show-allbtn').click(Map,Map.setAllMarkerMap);
-    $('.hide-allbtn').click(Map,Map.setAllMarkerNull);
-
+    $('.show-allbtn').click(this,this.setAllMarkerMap);
+    $('.hide-allbtn').click(this,this.setAllMarkerNull);
+    this.setAllMarkerMap();
   };
 
-  /*
-   *
+  /**
+  * @description [TODO] wirte comment
+  */
+  this.setAllMarkerMap = function () {
+    for ( var i = 0 , len = self.locationData().length ; i < len ; i++) {
+      self.locationData()[i].setMap(self.map);
+      self.bounds.extend(self.locationData()[i].latLng);
+    }
+    self.map.fitBounds(self.bounds);
+  };
+
+  /**
+  * @description [TODO] wirte comment
+  */
+  this.setAllMarkerNull = function (){
+    for ( var i = 0 , len = self.locationData().length ; i < len ; i++) {
+      self.locationData()[i].setMap(null);
+    }
+  };
+
+  /**
+  * @description [TODO] もうめんどくさいのでストリートビューなしで行こうかな
   */
   this.setStreetVeiwToInfoWindow = function (map,infowindow,marker,attachId) {
-    var mapApi =  google.maps // google.maps;
-
     if ( infowindow.marker !== marker ){
+      // set infowindow marker
       infowindow.marker = marker;
-
+      //init streetview service
       var streetViewService = new google.maps.StreetViewService();
-
       streetViewService.getPanoramaByLocation(marker.position, 50,function (data,status) {
-        if (status == mapApi.StreetViewStatus.OK){
-
+        if (status == google.maps.StreetViewStatus.OK){
           var location = data.location.latLng,
               panoramaOptions = {
                 position: location,
                 pov: {
-                  heading: mapApi.geometry.spherical.computeHeading(location, marker.position),
+                  heading: google.maps.geometry.spherical.computeHeading(location, marker.position),
                   pitch: 30
                 }
               },
               panorama = "";
-
           infowindow.setContent('<div>test ' + marker.title + '</div><div id="' + attachId + '"></div>');
-
-          var panorama = new mapApi.StreetViewPanorama( document.getElementById(attachId), panoramaOptions);
+          // attatch streetVIew to element
+          var panorama = new google.maps.StreetViewPanorama( document.getElementById(attachId), panoramaOptions);
         } else {
           infowindow.setContent('<div>' + marker.title + '</div>' +
             '<div>No Street View Found</div>');
@@ -241,8 +218,12 @@ function ViewModel () {
       });
     }
   };
-
 };
 
-var viewmodel = new ViewModel();
-// ko.applyBindings(viewmodel);
+// init function called by google map api callback
+function init() {
+  "use strict";
+  var viewmodel = new ViewModel();
+  ko.applyBindings(viewmodel);
+  viewmodel.init();
+};
